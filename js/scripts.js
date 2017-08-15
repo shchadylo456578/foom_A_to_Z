f09987f37adacacfc72790dcef3fcb6b9e5e2c72
shchadylo456578 <tarasdidg@gmail.com>
2017-08-15 13:39:03 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+/**
+ * Created by nazariibanakh on 5/3/17.
+ */
+
+var name = 'Test';
+
+name = name + 1;
+
+/**
+ * Created by nazariibanakh on 5/3/17.
+ */
+
+var name = 'Test2';
+
+name = name + 2;
